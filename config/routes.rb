ec4374043a815d0ec1dc8c234137b9eab684ff61
Saksham Pahwa <025tmpind@192.168.1.2>
2023-02-24 17:18:20 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'payment/checkout'
+  post 'payment/checkout'
+  resources :line_items
+  resources :carts
+  get 'gallery/index'
+  get 'gallery/search'
+  post 'gallery/search'
+  post 'gallery/index'
+  resources :stores
+  get 'admin/index'
+  get 'admin/login'
+  post 'admin/login'
+  get 'admin/logout'
+  get 'home/index'
+  root 'home#index'
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
